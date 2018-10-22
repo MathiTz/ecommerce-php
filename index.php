974368e78a976a75719794bc6d6af64fd52ac429
MathiTz <matheusalves789@gmail.com>
2018-10-21 21:35:54 -0300

    Tpl para página admin

--- index.php
+++ index.php
@@ -4,12 +4,13 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+$app->get('/admin', function() {
     
 	$page = new Page();
 

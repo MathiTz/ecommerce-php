ee9ef38d1ec75afc9b5d283c7b6842adf3795335
MathiTz <matheusalves789@gmail.com>
2018-10-25 04:01:26 -0300

    Implementando recuperar senha

--- index.php
+++ index.php
@@ -136,7 +136,30 @@ $app->post("/admin/users/:iduser", function($iduser){
 	
 });
 
+$app->get("/admin/forgot", function(){
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
 
+	$page->setTpl("forgot");
+});
+
+$app->post("/admin/forgot", function(){
+	
+	$user = User::getForgot($_POST["email"]);
+
+	header('location: /admin/forgot/sent');
+});
+
+$app->get("/admin/forgot/sent", function(){
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+});
 
 $app->run();
 

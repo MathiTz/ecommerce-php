e583a308b3102e1592d48b88123c9413da427a73
MathiTz <matheusalves789@gmail.com>
2018-11-03 19:15:11 -0300

    Corrigido erro na página create

--- admin-products.php
+++ admin-products.php
@@ -16,26 +16,12 @@ $app->get("/admin/products", function(){
     ]);
 });
 
-$app->get("/admin/products", function(){
-    User::verifyLogin();
-
-    $products = Product::listAll();
-
-    $page = new PageAdmin();
-
-    $page->setTpl("products", [
-        "products"=>$products
-    ]);
-});
-
 $app->get("/admin/products/create", function(){
     User::verifyLogin();
 
     $page = new PageAdmin();
 
-    $page->setTpl("products-create", [
-        
-    ]);
+    $page->setTpl("products-create");
 });
 
 $app->post("/admin/products/create", function(){

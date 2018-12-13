e71bec5a0f07baf7925700c2a401e8c802e724f9
MathiTz <matheusalves789@gmail.com>
2018-12-13 00:41:07 -0300

    Feito métodos para adição de itens no carrinho e conserto de erro sql

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -5,6 +5,7 @@ namespace Hcode\Model;
 use \Hcode\DB\Sql;
 use \Hcode\Model;
 use \Hcode\Mailer;
+use \Hcode\Model\User;
 
 class Cart extends Model {
 
@@ -26,12 +27,29 @@ class Cart extends Model {
                     'dessessionid'=>session_id()
                 ];
 
+                if (User::checkLogin(false)) {
+
+
                 $user = User::getFromSession();
 
+                $data['iduser'] = $user->getiduser();
+                    
+                }
+
+                $cart->setData($data);
+
+                $cart->save();
+
+                $cart->setToSession();
             }
         }
 
         return $cart;
+    }
+
+    public function setToSession()
+    {
+        $_SESSION[Cart::SESSION] = $this->getValues();
     }
 
     public function getFromSessionID()
@@ -78,8 +96,60 @@ class Cart extends Model {
         $this->setData($results[0]);
     }
 
+    public function addProduct(Product $product)
+    {
+        $sql =  new Sql();
+
+        $sql->query("INSERT INTO tb_cartsproducts(idcart, idproduct) VALUES(:idcart, :idproduct)", [
+            ':idcart'=>$this->getidcart(),
+            ':idproduct'=>$product->getidproduct()
+        ]);
+    }
+
+    public function removeProduct(Product $product, $all = false)
+    {
+
+        $sql = new Sql();
+
+        if ($all) {
+
+            $sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", [
+                ':idcart'=>$this->getidcart(),
+                ':idproduct'=>$product->getidproduct()
+            ]);
+        }
+
+        else {
+
+            $sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
+                ':idcart'=>$this->getidcart(),
+                ':idproduct'=>$product->getidproduct()
+            ]);
+
+        }
 
 
+    }
+
+    public function getProducts()
+    {
+
+        $sql = new Sql();
+
+        $rows = $sql->select("SELECT b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal 
+            FROM tb_cartsproducts a 
+            INNER JOIN tb_products b 
+            ON a.idproduct = b.idproduct 
+            WHERE a.idcart = :idcart 
+            AND a.dtremoved IS NULL 
+            GROUP BY b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl  
+            ORDER BY b.desproduct", [
+                ':idcart'=>$this->getidcart()
+            ]);
+
+        return Product::checkList($rows);
+
+    }
     
     
 }

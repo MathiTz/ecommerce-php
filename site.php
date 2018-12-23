30624cbf088abcaebe1aca6ca47b6956f1a0b15b
MathiTz <matheusalves789@gmail.com>
2018-12-23 15:31:29 -0300

    Correção de erro

--- site.php
+++ site.php
@@ -4,6 +4,8 @@ use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
 use \Hcode\Model\Cart;
+use \Hcode\Model\Address;
+use \Hcode\Model\User;
 
 $app->get('/', function() {
 
@@ -14,10 +16,11 @@ $app->get('/', function() {
 	$page->setTpl("index", [
 		'products'=>Product::checkList($products)
 	]);
+
 });
 
-$app->get('/categories/:idcategory', function($idcategory)
-{
+$app->get("/categories/:idcategory", function($idcategory){
+
 	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
 	$category = new Category();
@@ -28,7 +31,7 @@ $app->get('/categories/:idcategory', function($idcategory)
 
 	$pages = [];
 
-	for ($i=1 ; $i <= $pagination['pages']  ; $i++ ) {
+	for ($i=1; $i <= $pagination['pages']; $i++) {
 		array_push($pages, [
 			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
 			'page'=>$i
@@ -41,11 +44,12 @@ $app->get('/categories/:idcategory', function($idcategory)
 		'category'=>$category->getValues(),
 		'products'=>$pagination["data"],
 		'pages'=>$pages
-		]);
+	]);
 
 });
 
 $app->get("/products/:desurl", function($desurl){
+
 	$product = new Product();
 
 	$product->getFromURL($desurl);
@@ -56,6 +60,7 @@ $app->get("/products/:desurl", function($desurl){
 		'product'=>$product->getValues(),
 		'categories'=>$product->getCategories()
 	]);
+
 });
 
 $app->get("/cart", function(){
@@ -69,6 +74,7 @@ $app->get("/cart", function(){
 		'products'=>$cart->getProducts(),
 		'error'=>Cart::getMsgError()
 	]);
+
 });
 
 $app->get("/cart/:idproduct/add", function($idproduct){
@@ -79,15 +85,15 @@ $app->get("/cart/:idproduct/add", function($idproduct){
 
 	$cart = Cart::getFromSession();
 
-	$qtd = (isset($_GRT['qtd'])) ? (int)$_GET['qtd'] : 1;
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
 
-	for ($i = 0; $i < $qtd; $i++){
+	for ($i = 0; $i < $qtd; $i++) {
 
 		$cart->addProduct($product);
+
 	}
 
-
-	header("location: /cart");
+	header("Location: /cart");
 	exit;
 
 });
@@ -102,7 +108,7 @@ $app->get("/cart/:idproduct/minus", function($idproduct){
 
 	$cart->removeProduct($product);
 
-	header("location: /cart");
+	header("Location: /cart");
 	exit;
 
 });
@@ -117,7 +123,7 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 
 	$cart->removeProduct($product, true);
 
-	header("location: /cart");
+	header("Location: /cart");
 	exit;
 
 });
@@ -128,7 +134,216 @@ $app->post("/cart/freight", function(){
 
 	$cart->setFreight($_POST['zipcode']);
 
-	header('location: /cart');
+	header("Location: /cart");
+	exit;
+
+});
+
+$app->get("/checkout", function(){
+
+	User::verifyLogin(false);
+
+	$address = new Address();
+	$cart = Cart::getFromSession();
+
+	if (!isset($_GET['zipcode'])) {
+
+		$_GET['zipcode'] = $cart->getdeszipcode();
+
+	}
+
+	if (isset($_GET['zipcode'])) {
+
+		$address->loadFromCEP($_GET['zipcode']);
+
+		$cart->setdeszipcode($_GET['zipcode']);
+
+		$cart->save();
+
+		$cart->getCalculateTotal();
+
+	}
+
+	if (!$address->getdesaddress()) $address->setdesaddress('');
+	if (!$address->getdesnumber()) $address->setdesnumber('');
+	if (!$address->getdescomplement()) $address->setdescomplement('');
+	if (!$address->getdesdistrict()) $address->setdesdistrict('');
+	if (!$address->getdescity()) $address->setdescity('');
+	if (!$address->getdesstate()) $address->setdesstate('');
+	if (!$address->getdescountry()) $address->setdescountry('');
+	if (!$address->getdeszipcode()) $address->setdeszipcode('');
+
+	$page = new Page();
+
+	$page->setTpl("checkout", [
+		'cart'=>$cart->getValues(),
+		'address'=>$address->getValues(),
+		'products'=>$cart->getProducts(),
+		'error'=>Address::getMsgError()
+	]);
+
+});
+
+$app->post("/checkout", function(){
+
+	User::verifyLogin(false);
+
+	if (!isset($_POST['zipcode']) || $_POST['zipcode'] === '') {
+		Address::setMsgError("Informe o CEP.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if (!isset($_POST['desaddress']) || $_POST['desaddress'] === '') {
+		Address::setMsgError("Informe o endereço.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if (!isset($_POST['desdistrict']) || $_POST['desdistrict'] === '') {
+		Address::setMsgError("Informe o bairro.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if (!isset($_POST['descity']) || $_POST['descity'] === '') {
+		Address::setMsgError("Informe a cidade.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if (!isset($_POST['desstate']) || $_POST['desstate'] === '') {
+		Address::setMsgError("Informe o estado.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if (!isset($_POST['descountry']) || $_POST['descountry'] === '') {
+		Address::setMsgError("Informe o país.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	$user = User::getFromSession();
+
+	$address = new Address();
+
+	$_POST['deszipcode'] = $_POST['zipcode'];
+	$_POST['idperson'] = $user->getidperson();
+
+	$address->setData($_POST);
+
+	$address->save();
+
+	$cart = Cart::getFromSession();
+
+	$cart->getCalculateTotal();
+
+	$order = new Order();
+
+	$order->setData([
+		'idcart'=>$cart->getidcart(),
+		'idaddress'=>$address->getidaddress(),
+		'iduser'=>$user->getiduser(),
+		'idstatus'=>OrderStatus::EM_ABERTO,
+		'vltotal'=>$cart->getvltotal()
+	]);
+
+	$order->save();
+
+	switch ((int)$_POST['payment-method']) {
+
+		case 1:
+		header("Location: /order/".$order->getidorder()."/pagseguro");
+		break;
+
+		case 2:
+		header("Location: /order/".$order->getidorder()."/paypal");
+		break;
+
+	}
+
+	exit;
+
+});
+
+$app->get("/order/:idorder/pagseguro", function($idorder){
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = $order->getCart();
+
+	$page = new Page([
+		'header'=>false,
+		'footer'=>false
+	]);
+
+	$page->setTpl("payment-pagseguro", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts(),
+		'phone'=>[
+			'areaCode'=>substr($order->getnrphone(), 0, 2),
+			'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
+		]
+	]);
+
+
+});
+
+$app->get("/order/:idorder/paypal", function($idorder){
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = $order->getCart();
+
+	$page = new Page([
+		'header'=>false,
+		'footer'=>false
+	]);
+
+	$page->setTpl("payment-paypal", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
+	]);
+
+
+});
+
+$app->get("/login", function(){
+
+	$page = new Page();
+
+	$page->setTpl("login", [
+		'error'=>User::getError(),
+		'errorRegister'=>User::getErrorRegister(),
+		'registerValues'=>(isset($_SESSION['registerValues'])) ? $_SESSION['registerValues'] : ['name'=>'', 'email'=>'', 'phone'=>'']
+	]);
+
+});
+
+$app->post("/login", function(){
+
+	try {
+
+		User::login($_POST['login'], $_POST['password']);
+
+	} catch(Exception $e) {
+
+		User::setError($e->getMessage());
+
+	}
+
+	header("Location: /checkout");
 	exit;
 
 });
